c41c70f9537b937077a1c7a56ea942672a36e6dc
Daffa Aditya <79897029+daffadevhosting@users.noreply.github.com>
2025-02-22 14:22:51 +0700

    Update firebaseConfig

---
+++ assets/js/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCV3i_KliUAxYTI4RoC_VpFZxJ8-EjO2bA",
+  authDomain: "jekyll-onlineshop.firebaseapp.com",
+  databaseURL: "https://jekyll-onlineshop-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "jekyll-onlineshop",
+  storageBucket: "jekyll-onlineshop.firebasestorage.app",
+  messagingSenderId: "1080130094512",
+  appId: "1:1080130094512:web:fda6d8485c8d0ed6a78275",
+  measurementId: "G-75QG8P9ZSM"
+};
